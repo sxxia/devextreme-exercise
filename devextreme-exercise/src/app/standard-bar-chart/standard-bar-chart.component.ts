deab7deeeb6c1281ffa04e2335b12375003043dc
Shuang Xia <shuangx@stradigi.ai>
2019-07-17 10:41:47 -0400

    implement standard bar chart

--- devextreme-exercise/src/app/standard-bar-chart/standard-bar-chart.component.ts
+++ devextreme-exercise/src/app/standard-bar-chart/standard-bar-chart.component.ts
@@ -6,6 +6,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./standard-bar-chart.component.scss']
 })
 export class StandardBarChartComponent implements OnInit {
+  dataSource = [
+  {
+    day: "Monday",
+    oranges: 3
+  }, 
+  {
+    day: "Tuesday",
+    oranges: 2
+  }, 
+  {
+    day: "Wednesday",
+    oranges: 3
+  }, 
+  {
+    day: "Thursday",
+    oranges: 4
+  }, 
+  {
+    day: "Friday",
+    oranges: 6
+  }, 
+  {
+    day: "Saturday",
+    oranges: 11
+  }, 
+  {
+    day: "Sunday",
+    oranges: 4
+  }
+];
 
   constructor() { }
 
