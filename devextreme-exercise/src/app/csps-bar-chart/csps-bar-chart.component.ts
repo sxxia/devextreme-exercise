11c12bc1503391816a57060862d48b0b2a8ec03e
Shuang Xia <shuangx@stradigi.ai>
2019-09-10 09:20:43 -0400

    completed csps bar chart

--- devextreme-exercise/src/app/csps-bar-chart/csps-bar-chart.component.ts
+++ devextreme-exercise/src/app/csps-bar-chart/csps-bar-chart.component.ts
@@ -6,6 +6,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./csps-bar-chart.component.scss']
 })
 export class CSPSBarChartComponent implements OnInit {
+  dataSource = [{
+    field: "Industry 1",
+    value: 26
+  }, 
+  {
+    field: "Industry 2",
+    value: 60
+  }, 
+  {
+    field: "Industry 3",
+    value: 62
+  }, 
+  {
+    field: "Industry 4",
+    value: 88
+  }, 
+  {
+    field: "Industry 5",
+    value: 110
+  }, 
+  {
+    field: "Industry 6",
+    value: 32
+  }, 
+  {
+    field: "Industry 7",
+    value: 60
+  },
+  {
+    field: "Industry 8",
+    value: 89
+  },
+  {
+    field: "Industry 9",
+    value: 20
+  },
+  {
+    field: "Others",
+    value: 15
+  }
+];
 
   constructor() { }
 
